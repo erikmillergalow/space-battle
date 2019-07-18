138ff044231400f382251bfb56631ac99e310195
Erik Jurgen Miller-Galow <erikmillergalow@gmail.com>
2019-07-17 20:58:24 -0400

    Added ship movement and rotation
    Ship rotation looks wrong because ship image needs to be rotated

---
+++ Assets/Scripts/ship.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ship : MonoBehaviour
+{
+	public float movementSpeed = 10f;
+
+	// Rigidbody2D allows for easy physics-based gameplay
+	private Rigidbody2D body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = gameObject.GetComponent<Rigidbody2D>();
+
+        if (body == null) {
+        	Debug.LogError("Player::Start can't find RigidBody2D");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // FixedUpdate() is used for physics calculations and processed less than Update()
+    void FixedUpdate() {
+
+    	// check for player inputs
+    	if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) {
+    		float horizontalMovement = Input.GetAxisRaw("Horizontal") * movementSpeed;
+    		float verticalMovement = Input.GetAxisRaw("Vertical") * movementSpeed;
+
+    		Vector2 heading = new Vector2(horizontalMovement, verticalMovement);
+
+    		body.AddForce(heading);
+
+    		// point ship in direction of movement input
+    		transform.up = body.velocity;
+
+    	}
+    }
+}
