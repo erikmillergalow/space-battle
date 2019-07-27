ffe84883e62df2e04f11b4962d8724c38b4bf4bd
Erik Jurgen Miller-Galow <erikmillergalow@gmail.com>
2019-07-27 02:09:48 -0400

    Added basid projectile mechanics
    Need to fix the way that ship velocity is combined with bullet velocity
    
    Need to make bullets self-destroy when they hit other objects

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+
+	public int speed = 30;
+	public Vector3 targetVector;
+    public float shipVelocity;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	Rigidbody2D body = gameObject.GetComponentInChildren<Rigidbody2D>();
+        body.velocity = targetVector.normalized * speed * shipVelocity;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/ship.cs
+++ Assets/Scripts/ship.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ship : MonoBehaviour
+public class Ship : MonoBehaviour
 {
 	public float movementSpeed = 75f;
 
 	// Rigidbody2D allows for easy physics-based gameplay
 	private Rigidbody2D body;
+    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -22,7 +23,24 @@ public class ship : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // check for mouse clicks
+        if (Input.GetMouseButton(0)) {
+            Vector3 mouseVector = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseVector.z = 0; // working in 2D...
+
+            // create projectile and instance of script?
+            GameObject projectileObject = Instantiate(projectilePrefab, 
+                                                gameObject.transform.position, 
+                                                Quaternion.identity);
+            Projectile projectile = projectileObject.GetComponent<Projectile>();
+
+            // position of mouse click - position of player = direction of projectile
+            projectile.targetVector = mouseVector - gameObject.transform.position;
+
+            // alter velocity of projectile based on ship movement
+            projectile.shipVelocity = Vector3.Dot(body.velocity.normalized, transform.up.normalized); 
+
+        }
     }
 
     // FixedUpdate() is used for physics calculations and processed less than Update()
