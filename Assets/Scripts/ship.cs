05fab1c5dc9a56fc7536ad1078fd82a4f755c3f3
Erik Jurgen Miller-Galow <erikmillergalow@gmail.com>
2019-08-06 23:59:04 -0400

    Switched networking components to mirror, improved shooting slightly but it's broken in different ways now. Bullets not updating correctly in other instances of the game

--- Assets/Scripts/ship.cs
+++ Assets/Scripts/ship.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Networking;
+using Mirror;
 
 public class Ship : NetworkBehaviour
 {
@@ -31,26 +31,32 @@ public class Ship : NetworkBehaviour
 
         // check for mouse clicks
         if (Input.GetMouseButton(0)) {
-            Vector3 mouseVector = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            mouseVector.z = 0; // working in 2D...
-
-            // create projectile and instance of script?
-            GameObject projectileObject = Instantiate(projectilePrefab, 
-                                                      gameObject.transform.position, 
-                                                      Quaternion.identity);
-            Projectile projectile = projectileObject.GetComponent<Projectile>();
-
-            // position of mouse click - position of player = direction of projectile
-            projectile.targetVector = mouseVector - gameObject.transform.position;
-
-            // alter velocity of projectile based on ship movement
-            projectile.shipVelocityFactor = Vector3.Dot(body.velocity.normalized, 
-                                                        transform.up.normalized); 
-
-            // ignore collisions between shooter and projectile
-            Physics2D.IgnoreCollision(projectileObject.GetComponent<Collider2D>(), 
-                                    GetComponent<Collider2D>());
+            CmdShoot();
         }
+    }
+
+    [Command]
+    void CmdShoot() {
+
+        Vector3 mouseVector = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseVector.z = 0; // working in 2D...
+
+        // create projectile and instance of script?
+        GameObject projectileObject = Instantiate(projectilePrefab, 
+                                                  gameObject.transform.position, 
+                                                  Quaternion.identity);
+        Projectile projectile = projectileObject.GetComponent<Projectile>();
+
+        // position of mouse click - position of player = direction of projectile
+        projectile.targetVector = mouseVector - gameObject.transform.position;
+
+        // alter velocity of projectile based on ship movement
+        projectile.shipVelocityFactor = Vector3.Dot(body.velocity.normalized, 
+                                                        transform.up.normalized); 
+        // ignore collisions between shooter and projectile
+        Physics2D.IgnoreCollision(projectileObject.GetComponent<Collider2D>(), 
+                                  GetComponent<Collider2D>());
+        NetworkServer.Spawn(projectileObject);
     }
 
     // FixedUpdate() is used for physics calculations and processed less than Update()
