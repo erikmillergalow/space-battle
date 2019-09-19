4f3946416bed932cef8d2f121d0e8edf2ff0d06c
Erik Jurgen Miller-Galow <erikmillergalow@gmail.com>
2019-09-19 16:00:54 -0400

    Menu stars twinkle at random

---
+++ Assets/Scripts/MenuStar.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuStar : MonoBehaviour
+{
+
+	private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        //animator.enabled = false;
+        StartCoroutine("Flicker");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Flicker() {
+    	while (true) {
+	
+			//animator.enabled = false;
+			float delay = Random.Range(2.0f, 4.0f);
+			yield return new WaitForSeconds(delay);
+    		animator.Play("Twinkle", 0);
+    		//animator.enabled = true;
+    	}
+    }
+}
