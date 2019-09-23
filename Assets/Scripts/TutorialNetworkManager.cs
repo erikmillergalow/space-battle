8d1a219ae7b12ccbb14222efbf0cf1e4758050c6
Erik Jurgen Miller-Galow <erikmillergalow@gmail.com>
2019-09-22 20:18:09 -0400

    Added shooting instructions and automatic spawning to tutorial scene

---
+++ Assets/Scripts/TutorialNetworkManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class TutorialNetworkManager : MonoBehaviour
+{
+
+	private NetworkManager networkManager;
+
+    void Start() {
+        networkManager = GetComponent<NetworkManager>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    	// automatically start the server
+        if (!NetworkServer.active) {
+        	networkManager.StartHost();
+        }
+    }
+}
