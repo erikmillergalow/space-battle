b3fc43cd20a9895a67c0930db1278e74d936c981
Erik Jurgen Miller-Galow <erikmillergalow@gmail.com>
2019-10-03 20:08:07 -0400

    Shield fade behavior in new script

---
+++ Assets/Scripts/Shield.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+
+	private float fadeRate = 0.01f;
+	public SpriteRenderer sprite;
+
+    // Start is called before the first frame update
+    void Start() {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    	// shield fades away to transparent constantly
+        if (sprite.color.a > 0) {
+        	sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, sprite.color.a - fadeRate);
+        }
+    }
+}
